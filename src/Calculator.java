cd8cd183dbcdfad300787486e320c454e502fc95
aa23905 <aa23905@example.com>
2026-06-21 18:00:11 +0800

    feat: add calculator

---
+++ src/Calculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        //捕捉用户输入数字异常
+        try {
+            System.out.println("计算器，请输入一个数:");
+
+            double a = sc.nextDouble();
+            System.out.println("输入运算符号：+,-,*,/");
+            String operatorInput = sc.next();
+
+            if (operatorInput.length() != 1 || "+-*/".indexOf(operatorInput.charAt(0)) == -1) {
+                System.out.println("运算符号输入错误，请输入 + - * /");
+                return;
+            }
+            char operator = operatorInput.charAt(0);
+            System.out.println("输入第二个数:");
+            double b = sc.nextDouble();
+
+            switch (operator) {
+                case '+':
+                    System.out.println(a + b);
+                    break;
+                case '-':
+                    System.out.println(a - b);
+                    break;
+                case '*':
+                    System.out.println(a * b);
+                    break;
+                case '/':
+                    if (b == 0) {
+                        System.out.println("除数不能为0");
+                        return;
+                    }
+                    System.out.println(a / b);
+                    break;
+                default:
+                    System.out.println("输入错误");
+                    break;
+            }
+
+        } catch (Exception e) {
+            System.out.println("输入错误,请输入数字或运算符号");
+        } finally {
+            sc.close();
+        }
+
+
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,3 @@
+# Java 基础语法练习
+
+- Calculator.java：命令行计算器（加减乘除）
